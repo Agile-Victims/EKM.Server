0a847ffdb12f7117ef3c961e31984b09727506a0
osman hayek <114689692+osmanhayek@users.noreply.github.com>
2025-03-26 04:22:34 +0300

    Added Swing ui for better test options

--- src/main/java/agile/victims/EKM/Server/controller/StudentController.java
+++ src/main/java/agile/victims/EKM/Server/controller/StudentController.java
@@ -1,5 +1,24 @@
 package agile.victims.EKM.Server.controller;
 
+import agile.victims.EKM.Server.entity.Student;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@Tag(name = "EKM server", description = "API's For Software Engineering With Agile Class")
 public class StudentController {
-    
+
+    @GetMapping("/students")
+    @Operation(summary = "get all students", description = "Returns string")
+    public String getAllStudents() {
+        return "All students";
+    }
+
+    @PostMapping("/students")
+    @Operation(summary = "Create a user", description = "Returns string")
+    public String createStudent(@RequestBody Student student) {
+        return "Student created";
+    }
 }
